00a0ca62e5da61b8c6cdee7a866cb398433dfec0
Izzy <izzyvalorant.fb@gmail.com>
2025-11-20 13:44:49 +0800

    Initial commit

---
+++ scripts/killzone.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+@onready var timer: Timer = $Timer
+@onready var death_sound: AudioStreamPlayer2D = $"death sound"
+
+func _on_body_entered(body: Node2D) -> void:
+	print("You died!")
+	death_sound.play()
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
+	timer.start()
+
+func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0
+	get_tree().reload_current_scene() # Replace with function body.
