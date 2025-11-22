94ea83c95d50db247183f94b962518a1713abdf8
Izzy <izzyvalorant.fb@gmail.com>
2025-11-22 17:52:01 +0800

    Fixed a bug
    Fixed a bug for the key and portal when going to the next level

--- LevelManager.gd
+++ LevelManager.gd
@@ -3,6 +3,7 @@ extends Node
 var current_level: int = 0   # starts at Level1.tscn
 
 func goto_next_level():
+	GameState.haveKey = false
 	current_level += 1
 	var path := "res://levels/level_%d.tscn" % current_level
 	get_tree().change_scene_to_file(path)
