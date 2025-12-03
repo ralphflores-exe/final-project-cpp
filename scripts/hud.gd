a63a5837d0f3188ffa35de667b6466f2701f14f5
Izzy <izzyvalorant.fb@gmail.com>
2025-12-04 01:12:48 +0800

    Add level scripts and stopwatch to project
    Added new GDScript files for level_1, level_4, and stopwatch, and integrated level scripts into their respective scenes. Updated editor and project configuration to reflect new scripts and scene changes, including HUD and player script references.

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var label: Label = $Label
+@onready var label_2: Label = $Label2
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,3 +12,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	label.text = "Deaths: %d" % GameState.deaths
+	
+	var t = Stopwatch.time_elapsed
+	var minutes = int(t / 60)
+	var seconds = int(t) % 60
+	var ms = int((t - int(t)) * 100)
+	
+	label_2.text = "Time: %02d:%02d.%02d" % [minutes, seconds, ms]
+	
