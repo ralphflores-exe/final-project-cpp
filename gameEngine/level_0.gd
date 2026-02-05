b85f35c7f06a4ec89ee01d406582557b11aa4aa5
Izzy <izzyvalorant.fb@gmail.com>
2026-02-05 21:07:54 +0800

    Add Windows build & MainMenu, update Godot editor
    Adds Windows build artifacts (VoidJumper.exe, Jenova.Runtime.Win64.dll) and imports MainMenu.png (+ .import). Removes several GDScript sources under scripts/ and updates native/C++ script metadata (.uid). Updates Godot editor state and caches (.godot/editor/*, filesystem_cache10, uid_cache) to reflect opened scenes, selected script, and new/updated imports; also updates various level/scene files and gameEngine script metadata.

--- gameEngine/level_0.gd
+++ gameEngine/level_0.gd
@@ -1,30 +1,6 @@
 extends Control
-@onready var start: Button = $NinePatchRect/VBoxContainer/Start
-@onready var options: Button = $NinePatchRect/VBoxContainer/Options
-@onready var quit: Button = $NinePatchRect/VBoxContainer/Quit
-@onready var press_sound: AudioStreamPlayer2D = $pressSound
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-var pending_action := ""
 
 func _ready():
-	# Connect buttons to functions
-	$NinePatchRect/VBoxContainer/Start.pressed.connect(_on_start_game_pressed)
-	$NinePatchRect/VBoxContainer/Quit.pressed.connect(_on_quit_pressed)
-	
-	press_sound.finished.connect(_on_press_sound_finished)
-	
-func _on_start_game_pressed():
-	pending_action = "start"
-	press_sound.play()
-
-func _on_quit_pressed():
-	pending_action = "quit"
-	press_sound.play()
-	
-func _on_press_sound_finished():
-	if pending_action == "start":
-		LevelManager.goto_next_level()
-	elif pending_action == "quit":
-		get_tree().quit()
-		
-	pending_action = ""
+	animation_player.play("fade_in")
